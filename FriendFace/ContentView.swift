21a00b75476265e7a99fc3d256a020deca86f380
Brandon Knox <bknox.dev@gmail.com>
2021-05-01 14:52:24 -0700

    Added loadData function to pull and parse JSON data.  Created view to list name and company attributes.

--- FriendFace/ContentView.swift
+++ FriendFace/ContentView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct User {
+struct User: Codable {
     var id: String
     var isActive: Bool
     var name: String
@@ -17,20 +17,54 @@ struct User {
     var about: String
     var registered: String  // change to date later
     var tags: [String]
+    var friends: [Friend]
 }
 
-struct Friend {
+struct Friend: Codable {
     var id: String
     var name: String
 }
 
 struct ContentView: View {
-    // import JSON
+    @State var users = [User]()
     
     var body: some View {
         // create list view to display JSON
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(users, id: \.id) { item in
+                VStack(alignment: .leading) {
+                    Text(item.name)
+                        .font(.headline)
+                    
+                    Text(item.company)
+                }
+            }
+        }
+        .onAppear(perform: loadData)
+    }
+    
+    func loadData() {
+        guard let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json") else {
+            print("Invalid URL")
+            return
+        }
+        
+        let request = URLRequest(url: url)
+
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+//                let jsonData = jsonString.data(using: .utf8)!
+                if let decodedResponse = try?JSONDecoder().decode([User].self, from: data) {
+                    DispatchQueue.main.async {
+                        self.users = decodedResponse
+                    }
+                    print("Results returned")
+                    return
+                }
+            }
+            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+            print(request)
+        }.resume()
     }
 }
 
